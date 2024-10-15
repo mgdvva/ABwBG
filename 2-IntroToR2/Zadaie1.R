ba8a670c9a141531a0051278718a970e98abc27a
mgdvva <magda.jackiewicz96@gmail.com>
2024-10-15 10:58:48 +0200

    Add files via upload

---
+++ 2-IntroToR2/Zadaie1.R
@@ -0,0 +1,16 @@
+### Instrukcje i rozwiązanie:
+# ------------Komentarze--------------
+  4.  **W skrypcie napisz komentarze** opisujące funkcje poszczególnych paneli.
+# konsola (console) - wykonywanie skryptu 
+# edytor skryptów (spource) - pisanie skryptów, funkcje nie są automatycznie wykonywane
+# środowisko (global environment) - tam są pokazywane wszystkie przypisane wartości
+# panel wyjściowy - tam są rzeczy, które ułatwiające w Rsdtufio (np.import plików)
+
+#------------------Motyw------------------
+5.  **Znajdź w RStudio ustawienia** i **zmień motyw kolorystyczny** na taki, który najbardziej Ci odpowiada.
+
+6.  **Napisz w komentarzu**, jaki motyw wybrałeś/aś i dlaczego.
+# Wybrałam motyw Dracula,bo jest ciemny z elementami różowego-ładny i głowanie boli 
+7.  **Zapisz skrypt** jako `zadanie1.R` w swoim folderze.
+
+8.  **Prześlij plik do swojego repozytorium na GitHub**.
